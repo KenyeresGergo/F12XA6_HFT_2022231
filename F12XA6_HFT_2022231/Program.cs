e73f900f41183ffb05270b26200b6f36c28105eb
Kenyeres Gergő <keny.gergo@gmail.com>
2022-10-19 12:20:47 +0200

    Add project files.

---
+++ F12XA6_HFT_2022231/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace F12XA6_HFT_2022231
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
