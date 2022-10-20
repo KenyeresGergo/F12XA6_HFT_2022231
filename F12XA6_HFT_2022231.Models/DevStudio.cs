1fd3e1521e0d64b15d073f4bfe2651774a935f14
Kenyeres Gergő <keny.gergo@gmail.com>
2022-10-20 12:11:04 +0200

    DevStudio calss added to models

---
+++ F12XA6_HFT_2022231.Models/DevStudio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F12XA6_HFT_2022231.Models
+{
+    [Table("Developer Studios")]
+    internal class DevStudio :IDbEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Game> Games { get; set; }
+
+        public ICollection<Developer> Developers { get; set; }
+    }
+}
