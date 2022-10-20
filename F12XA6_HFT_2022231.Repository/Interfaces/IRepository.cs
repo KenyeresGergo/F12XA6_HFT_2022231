0ff587310de9f6ad69f598664d52629a1981fe31
Kenyeres Gergő <keny.gergo@gmail.com>
2022-10-20 15:49:24 +0200

    IREpository created

---
+++ F12XA6_HFT_2022231.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F12XA6_HFT_2022231.Repository.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        void Create(T item);
+        public T Read(int id);
+        IQueryable<T> ReadAll();
+        void Update(T item);
+        void Delete(int id);
+
+    }
+}
