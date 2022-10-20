ddbd28905c3086d39c9b2e3c74f28d2ec88bd6f9
Kenyeres Gergő <keny.gergo@gmail.com>
2022-10-20 12:08:30 +0200

    Game calss added

---
+++ F12XA6_HFT_2022231.Models/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F12XA6_HFT_2022231.Models
+{
+    [Table("Games")]
+    internal class Game : IDbEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name  { get; set; }
+
+        public int Price { get; set; }
+
+        public string DevStudio { get; set; }
+
+
+    }
+}
