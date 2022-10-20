a5e130a64b2a4fe1c400392d8a16ee743dd69e4d
Kenyeres Gergő <keny.gergo@gmail.com>
2022-10-20 12:14:56 +0200

    Developer class added to Models

---
+++ F12XA6_HFT_2022231.Models/Developer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F12XA6_HFT_2022231.Models
+{
+    [Table("Developers")]
+    internal class Developer : IDbEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DevStudio Company { get; set; }
+
+        public int Salary { get; set; }
+
+
+    }
+}
