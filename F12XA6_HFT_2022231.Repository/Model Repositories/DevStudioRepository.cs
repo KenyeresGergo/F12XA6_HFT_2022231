cd867816bc67659b1dfdb524ddf5da4ddf248701
Kenyeres Gergő <keny.gergo@gmail.com>
2022-10-30 11:09:00 +0100

    Model Repositories added

---
+++ F12XA6_HFT_2022231.Repository/Model Repositories/DevStudioRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F12XA6_HFT_2022231.Repository
+{
+    internal class DevStudioRepository
+    {
+    }
+}
