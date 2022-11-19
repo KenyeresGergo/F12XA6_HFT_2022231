bf7389d5125c69d77555c8e04d0fbdf188afbed3
Kenyeres Gergő <keny.gergo@stud.uni-obuda.hu>
2022-11-19 11:32:43 +0100

    ValamiGamesByStudioName added

--- F12XA6_HFT_2022231.Logic/ModelLogics/DevStudioLogic.cs
+++ F12XA6_HFT_2022231.Logic/ModelLogics/DevStudioLogic.cs
@@ -6,30 +6,24 @@ using System.Threading.Tasks;
 using F12XA6_HFT_2022231.Models;
 using F12XA6_HFT_2022231.Repository.Interfaces;
 using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using static F12XA6_HFT_2022231.Logic.ModelLogics.DeveloperLogic;
 
 namespace F12XA6_HFT_2022231.Logic
 {
     public class DevStudioLogic
     {
-        private IRepository<DevStudio> reop;
+        private IRepository<DevStudio> repo;
 
         public DevStudioLogic(IRepository<DevStudio> reop)
         {
-            this.reop = reop;
+            this.repo = reop;
         }
-
-        //public IEnumerable<StudiInfo> GameTytlesByStudio()
-        //{
-        //    var res = from x in reop.ReadAll()
-        //        group x by x.StudioName
-        //        into g
-        //        select new StudiInfo()
-        //        {
-        //            StudioName = g.Key,
-        //            GameTytles = g.SelectMany(t => t.Games.Where(k => k.GameTitle))
-        //        };
-        //    return res;
-        //}
+        public IEnumerable<ICollection<Game>> ValamiGamesByStudioName(string studioname) //Returns a list that contains the name of workers for a studio
+        {
+            var res =
+                repo.ReadAll().Where(t => t.StudioName == studioname).Select(t => t.Games);
+            return res;
+        }
 
         public class StudiInfo
         {
--- F12XA6_HFT_2022231.Logic/ModelLogics/DeveloperLogic.cs
+++ F12XA6_HFT_2022231.Logic/ModelLogics/DeveloperLogic.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using F12XA6_HFT_2022231.Models;
+using F12XA6_HFT_2022231.Repository.Interfaces;
 
 namespace F12XA6_HFT_2022231.Logic.ModelLogics
 {
-    internal class DeveloperLogic
+    public class DeveloperLogic
     {
+        private IRepository<Developer> repo;
+
+        public DeveloperLogic(IRepository<Developer> repo)
+        {
+            this.repo = repo;
+        }
+
+
+        
+
+
+        public class DeveloperInfo
+        {
+            public string CompanyName { get; set; }
+
+
+        }
     }
 }
