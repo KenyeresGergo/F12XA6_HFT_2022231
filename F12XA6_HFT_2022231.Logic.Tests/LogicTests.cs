c8d2696fd397357a38069c322c0b629bf297df52
Kenyeres Gergő <keny.gergo@stud.uni-obuda.hu>
2022-11-20 14:41:33 +0100

    GameCountByStudioWithRightInput added

--- F12XA6_HFT_2022231.Logic.Tests/LogicTests.cs
+++ F12XA6_HFT_2022231.Logic.Tests/LogicTests.cs
@@ -20,36 +20,34 @@ namespace F12XA6_HFT_2022231.Logic.Tests
         [SetUp]
         public void Init()
         {
-            #region Demo data for Dev Studio Lists
-            var dg1 = new Game { Id = 1, GameTitle = "Cyberpunk2077", Price = 60, Rating = 2 };
-            var dg2 = new Game { Id = 2, GameTitle = "Grand Theft Auto V", Price = 50, Rating = 7 };
-            var dg3 = new Game { Id = 3, GameTitle = "Grand Theft Auto IV", Price = 30, Rating = 6 };
-            var dg4 = new Game { Id = 4, GameTitle = "The Witcher 3: Wild hunt", Price = 55, Rating = 4 };
+            #region Mock Data
 
-            var de1 = new Developer { Id = 2, DevName = "Carter", Salary = 64000 };
-            var de2 = new Developer { Id = 4, DevName = "Mike", Salary = 82000 };
-            var de3 = new Developer { Id = 6, DevName = "Noah", Salary = 120000 };
-            #endregion
-
-
-            var a = new DevStudio { Id = 1, StudioName = "CD Pojekt RED", Games = new List<Game> { dg1, dg4 }, Employees = new List<Developer> { de1, de2 } };
-            var b = new DevStudio { Id = 2, StudioName = "Rockstar Games", Games = new List<Game> { dg2, dg3 }, Employees = new List<Developer> { de3 } };
+            var a = new DevStudio { Id = 1, StudioName = "CD Pojekt RED", Games = new List<Game>(), Employees = new List<Developer>() };
+            var b = new DevStudio { Id = 2, StudioName = "Rockstar Games", Games = new List<Game>(), Employees = new List<Developer>() };
             var studios = new List<DevStudio> { a, b };
 
-            var games = new List<Game>
-            {
-               new Game { Id = 1, GameTitle = "Cyberpunk2077", Price = 60, PublisherStudio = a, Rating = 2 },
-               new Game { Id = 2, GameTitle = "Grand Theft Auto V", Price = 50, PublisherStudio = b, Rating = 7 },
-               new Game { Id = 3, GameTitle = "Grand Theft Auto IV", Price = 30, PublisherStudio = b, Rating = 6 },
-               new Game { Id = 4, GameTitle = "The Witcher 3: Wild hunt", Price = 55, PublisherStudio = a, Rating = 4 }
-            };
 
-            var devs = new List<Developer>
-            {
-                new Developer{Id = 2,DevName = "Carter",Company = a,Salary = 64000},
-                new Developer{Id = 4,DevName = "Mike",Company = a,Salary = 82000},
-                new Developer{Id = 6,DevName = "Noah",Company = b,Salary = 120000},
-            };
+            var g1 = new Game { Id = 1, GameTitle = "Cyberpunk2077", PublisherStudio = a, Price = 60, Rating = 2 };
+            var g2 = new Game { Id = 2, GameTitle = "Grand Theft Auto V", PublisherStudio = b, Price = 50, Rating = 7 };
+            var g3 = new Game { Id = 3, GameTitle = "Grand Theft Auto IV", PublisherStudio = b, Rating = 6 };
+            var g4 = new Game { Id = 4, GameTitle = "The Witcher 3: Wild hunt", PublisherStudio = a, Price = 55, Rating = 4 };
+            var games = new List<Game> { g1, g2, g3, g4 };
+
+            var e1 = new Developer { Id = 2, DevName = "Carter", Company = a, Salary = 64000 };
+            var e2 = new Developer { Id = 4, DevName = "Mike", Company = a, Salary = 82000 };
+            var e3 = new Developer { Id = 6, DevName = "Noah", Company = b, Salary = 120000 };
+            var devs = new List<Developer> { e1, e2, e3 };
+
+            a.Games.Add(g1);
+            a.Games.Add(g4);
+            a.Employees.Add(e1);
+            a.Employees.Add(e2);
+
+            b.Games.Add(g2);
+            b.Games.Add(g3);
+            b.Employees.Add(e3);
+
+            #endregion
 
             mockStudioRepo = new Mock<IRepository<DevStudio>>();
             mockStudioRepo.Setup(m => m.ReadAll()).Returns(studios);
@@ -62,6 +60,20 @@ namespace F12XA6_HFT_2022231.Logic.Tests
 
         }
 
+        [Test]
+        public void GameCountByStudioWithRightInput()
+        {
+
+            var logic = new GameLogic(mockGameRepo.Object);
+
+            var res = logic.GameCountByStudio();
+
+            Assert.That(res.First().GameCount, Is.EqualTo(2));
+            Assert.That(res.First().PublisherStudioName, Is.EqualTo("CD Pojekt RED"));
+            Assert.That(res.ElementAt(1).GameCount, Is.EqualTo(2));
+            Assert.That(res.ElementAt(1).PublisherStudioName, Is.EqualTo("Rockstar Games"));
+
+        }
         [Test]
         public void AvgRatingByStudioWithRightInput()
         {
