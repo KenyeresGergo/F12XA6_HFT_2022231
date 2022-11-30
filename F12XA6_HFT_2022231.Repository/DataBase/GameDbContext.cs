1e3bee31cf51e835caac60d40075a46a55d973e5
Kenyeres Gergő <keny.gergo@stud.uni-obuda.hu>
2022-11-30 17:09:52 +0100

    Datebase Extended

--- F12XA6_HFT_2022231.Repository/DataBase/GameDbContext.cs
+++ F12XA6_HFT_2022231.Repository/DataBase/GameDbContext.cs
@@ -63,7 +63,26 @@ namespace F12XA6_HFT_2022231.Repository
                     new Game(8,"Just Cause 4", 35, 5,8),
                     new Game(9,"Valorant", 0, 6,4),
                     new Game(10,"Call of Duty 4: Modern Warfare", 10, 7, 9),
-                    new Game(11,"Sekiro: Shadows Die Twice", 70,8,7)
+                    new Game(11,"Sekiro: Shadows Die Twice", 70,8,7),
+                    new Game(12,"Resident Evil 2", 55,9,8),
+                    new Game(13,"Left 4 Dead 2", 5,10,9),
+                    new Game(14,"Star Wars Battlefront II", 42,11,6),
+                    new Game(15,"Among Us", 0,12,2),
+                    new Game(16,"BioShock Infinite", 15,13,10),
+                    new Game(17,"Borderlands 2", 30,14,10),
+                    new Game(18,"Shadow of the Tomb Raider", 10,5,8),
+                    new Game(19,"Watch Dogs 2", 50,4,3),
+                    new Game(20,"World War Z", 42,16,7),
+                    new Game(21,"Devil May Cry 5", 25,9,9),
+                    new Game(22,"Metal Gear Solid V: The Phantom Pain", 38,17,9),
+                    new Game(23,"Silent Hill", 26,17,4),
+                    new Game(24,"Red Dead Redemption 2", 65,2,9),
+                    new Game(25,"Assassin's Creed Odyssey", 60,4,6),
+                    new Game(25,"For Honor", 50,4,5),
+                    new Game(25,"Tom Clancy's The Division", 70,4,8),
+                    new Game(25,"TCrash Team Racing Nitro-Fueled", 40,8,6),
+                    new Game(25,"NBA 2K22", 70,13,8),
+                    
                     
                 }
             );
@@ -76,22 +95,31 @@ namespace F12XA6_HFT_2022231.Repository
                     new DevStudio(5,"Avalanche Studios Group"),
                     new DevStudio(6,"Riot Games"),
                     new DevStudio(7,"Infiniti Ward"),
-                    new DevStudio(8,"Activision")
+                    new DevStudio(8,"Activision"),
+                    new DevStudio(9,"Capcom"),
+                    new DevStudio(10,"Valve Co."),
+                    new DevStudio(11,"Gordy Haab"),
+                    new DevStudio(12,"InnerSloth LLC"),
+                    new DevStudio(13,"2K Games"),
+                    new DevStudio(14,"Gearbox Sofware"),
+                    new DevStudio(15,"Square Enix"),
+                    new DevStudio(16,"Saber Interactive"),
+                    new DevStudio(17,"Kojima Productions"),
 
                 }
             );
             modelBuilder.Entity<Developer>().HasData(new Developer[]
                 {
                     new Developer(1,"John",4,64000),
-                    new Developer(2,"Cater",1,64000),
-                    new Developer(3,"Monica",4,64000),
-                    new Developer(4,"Mike",1,64000),
-                    new Developer(5,"Liam",3,64000),
-                    new Developer(6,"Noah",2,64000),
-                    new Developer(7,"Emma",6,64000),
-                    new Developer(8,"Olivia",7,64000),
-                    new Developer(9,"Henry",8,64000),
-                    new Developer(10,"James",5,64000),
+                    new Developer(2,"Cater",1,128000),
+                    new Developer(3,"Monica",4,32000),
+                    new Developer(4,"Mike",1,78000),
+                    new Developer(5,"Liam",3,69000),
+                    new Developer(6,"Noah",2,84000),
+                    new Developer(7,"Emma",6,92000),
+                    new Developer(8,"Olivia",7,87000),
+                    new Developer(9,"Henry",8,66000),
+                    new Developer(10,"James",5,37000),
 
                 }
             );
